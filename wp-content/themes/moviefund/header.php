10b4ce4d62f630b179472703acc6326fd13417da
Franz Noel Tanglao <franz@asteriainteractive.com>
2016-02-20 15:54:16 -0800

    Removed how it works on members page.

--- wp-content/themes/moviefund/header.php
+++ wp-content/themes/moviefund/header.php
@@ -217,47 +217,49 @@
         <?php get_template_part('part','filmpanel'); ?>
         <?php // get_template_part('part','share'); ?>
 
-        <div class="how-it-works text-center" id="how">
-            <div class="container">
-                <h2 class="white">How It Works</h2>
-                <div class="button">
-                    <ul class="list-inline">
-                        <li><a class="btn btn-7">FILM MAKERS</a></li>
-                        <li><a class="btn btn-5 active-ihwit"> INVESTORS</a></li>
-                    </ul>
-                </div>
-                <div class="row">
-                    <div class="mob-center lar-view">
-                        <div class="col-md-12">
-                            <div class="how_steps set-posi1">
-                               <div class="slider4">
-                               <?php how_it_inves();?>
+        <?php if (!is_page("members")): ?>
+            <div class="how-it-works text-center" id="how">
+                <div class="container">
+                    <h2 class="white">How It Works</h2>
+                    <div class="button">
+                        <ul class="list-inline">
+                            <li><a class="btn btn-7">FILM MAKERS</a></li>
+                            <li><a class="btn btn-5 active-ihwit"> INVESTORS</a></li>
+                        </ul>
+                    </div>
+                    <div class="row">
+                        <div class="mob-center lar-view">
+                            <div class="col-md-12">
+                                <div class="how_steps set-posi1">
+                                   <div class="slider4">
+                                   <?php how_it_inves();?>
+                                   </div>
+                                   </div>
+                                   <div class="how_steps set-posi2 hw-hide">
+                                   <div class="slider4">
+                                   <?php how_it();?>
+                                   </div>
                                </div>
+                            </div>
+                        </div>
+                        <div class="mob-center mob-view">
+                            <div class="col-md-12">
+                               <div class="how_steps set-posi1">
+                                    <div class="slider2">
+                                       <?php how_it_inves();?>
+                                    </div>  
                                </div>
                                <div class="how_steps set-posi2 hw-hide">
-                               <div class="slider4">
-                               <?php how_it();?>
+                                   <div class="slider2">
+                                       <?php how_it();?>
+                                   </div>
                                </div>
-                           </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="mob-center mob-view">
-                        <div class="col-md-12">
-                           <div class="how_steps set-posi1">
-                                <div class="slider2">
-                                   <?php how_it_inves();?>
-                                </div>  
-                           </div>
-                           <div class="how_steps set-posi2 hw-hide">
-                               <div class="slider2">
-                                   <?php how_it();?>
-                               </div>  
-                           </div>                      
-                        </div>
-                    </div>             
                 </div>
             </div>
-        </div>
+        <?php endif; ?>
     </div>
     <div class="content">
 <?php } ?> 
